b30fe06aab4b3296e6200b10f703347748b4bcf0
davidclose322 <80085254+davidclose322@users.noreply.github.com>
2021-03-25 18:49:07 +0800

    Add files via upload

---
+++ 第三次作业(协作完成)/BMPUnixImp.java
@@ -0,0 +1,6 @@
+//Unix操作系统实现BMP图像：具体实现类
+class BMPUnixImp implements BMPImage {
+    public void doPaint(Matrix m) {
+        //调用Unix系统的绘制函数绘制像素矩阵
+        System.out.print("在Unix操作系统中显示BMP图像：");
+    }
---
+++ 第三次作业(协作完成)/BMPWindowsImp.java
@@ -0,0 +1,6 @@
+//Windows操作系统实现BMP图像：具体实现类
+class BMPWindowsImp implements BMPImage {
+    public void doPaint(Matrix m) {
+        //调用Windows系统的绘制函数绘制像素矩阵
+        System.out.print("在Windows操作系统中显示BMP图像：");
+    }
